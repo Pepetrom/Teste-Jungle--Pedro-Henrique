f4fc9ca05bcec0c18d5af56922704ff0012c0912
Pedro Henrique Silva Vieira <pedrohsvnegocios@gmail.com>
2026-09-18 15:39:23 -0300

    vercel não está cooperando

--- src/game/Entities.ts
+++ src/game/Entities.ts
@@ -245,12 +245,15 @@ export class Explosion extends Entity {
     super(texs, container);
     this.x = x;
     this.y = y;
+    this.sprite.x = x;
+    this.sprite.y = y;
     
     const animSprite = this.sprite as PIXI.AnimatedSprite;
     animSprite.animationSpeed = 0.15;
     animSprite.loop = false;
     animSprite.onComplete = () => {
       this.isDead = true;
+      this.sprite.destroy();
     };
     animSprite.play();
 
